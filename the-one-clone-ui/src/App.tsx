85a2dd7df9386eafa916a4fd1be3e4206ac1f313
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2021-01-07 22:58:13 +0000

    handle on ready state

--- the-one-clone-ui/src/App.tsx
+++ the-one-clone-ui/src/App.tsx
@@ -1,7 +1,6 @@
-import React, {ChangeEvent, useContext, useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
-import io, {Manager, Socket} from 'socket.io-client'
-import axios from 'axios'
+import io, {Socket} from 'socket.io-client'
 import {BrowserRouter as Router, Route, Switch, useLocation} from 'react-router-dom'
 
 function useQuery() {
@@ -21,37 +20,66 @@ function App() {
     );
 }
 
+interface Player {
+    id: string
+    name: string
+    isReady: boolean
+    isMe: boolean
+    isAdmin: boolean
+}
+
 function Home() {
     const [playerName, setPlayerName] = useState("");
     const [room, setRoom] = useState()
     let query = useQuery();
     const [socket, setSocket] = useState<Socket>();
     const [roomId, setRoomId] = useState("09zzt1lym3")
-    const [players, setPlayers] = useState<any []>()
+    const [players, setPlayers] = useState<Player[]>()
+    const [me, setMe] = useState<Player>()
+
+    useEffect(() => {
+        function toPlayers(playersJoined: any[]) {
+            const toPlayer = (player: any) => {
+                return {
+                    id: player.id,
+                    name: player.playerName,
+                    isReady: player.isReady,
+                    isMe: player.id === socket!.id,
+                    isAdmin: player.isAdmin
+                }
+            }
+            return playersJoined.map(player => toPlayer(player));
+        }
+
+        if (socket) {
+            socket.on('connect',  () => {
+                console.log(socket)
+            })
+
+            socket.on('show-players',  (data: {playersJoined: any[]}) => {
+                console.log(data)
+                setPlayers(toPlayers(data.playersJoined))
+                setMe(data.playersJoined.find(player => player.id === socket.id))
+            })
+            socket.on('start-game',  (data:any) => {
+                console.log(data)
+            })
+        }
+        if (players) {
+            setMe(players.find(player => player.isMe))
+        }
+    }, [socket])
 
 
+    const connectWebsocket = (action: string) => {
+        // @ts-ignore
+        setSocket(io(`ws://localhost:3000`, {forceNew: false, query: {roomId, playerName, action}}));
+    }
 
     // const roomId = query.get("room-id")
     const onJoinRoom = async () => {
         const action = "join"
-        // @ts-ignore
-        let socketInstance = io(`ws://localhost:3000`, {forceNew: false, query: { roomId, playerName, action }});
-        socketInstance.on('connect',  () => {
-            console.log(socketInstance)
-        })
-
-        socketInstance.on('show-players',  (data: {playersJoined: any[]}) => {
-            console.log(data)
-            setPlayers(data.playersJoined)
-        })
-        // socketInstance.on('conn-ack',  (data: any) => {
-        //     console.log("from conn-ack...")
-        //     console.log(socketInstance)
-        //     console.log(socketInstance!.connected)
-        //     console.log(data)
-        // })
-        setSocket(socketInstance);
-
+        connectWebsocket(action)
     };
 
     const onCreateRoom = async () => {
@@ -59,14 +87,17 @@ function Home() {
         // const roomId = Math.random().toString(36).replace(/[^\w]+/g, '').substr(0, 10)
         // setRoomId(roomId)
         console.log(`roomId: ${roomId}`)
-        // @ts-ignore
-        setSocket(io(`ws://localhost:3000`, {forceNew: false, query: { roomId, playerName, action }}));
-
-
+        connectWebsocket(action)
     };
+
     const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         setPlayerName(event.target.value || "")
     };
+
+    const onReady = () => {
+        !me?.isReady ? socket!.emit('on-ready') : socket!.emit('on-not-ready')
+    }
+
     return (
         <div>
             <input value={playerName} onChange={onInputChange} type="text" placeholder={"Player Name"}/>
@@ -76,7 +107,13 @@ function Home() {
                 <button onClick={onCreateRoom}>Create Room</button>
             {/*}*/}
             <div>
-                {players?.map(player => <li key={player.id}>id: {player.id}, name: {player.playerName}</li>)}
+                Me: {JSON.stringify(me)}
+            </div>
+            <div>
+                {players?.map(player => <li key={player.id}>id: {player.id}, name: {player.name} ready?: {player.isReady.toString()}</li>)}
+            </div>
+            <div>
+                {me && <button onClick={onReady}>{me.isReady ? 'Not Ready' : 'I\'m Ready'}</button>}
             </div>
         </div>
     )
