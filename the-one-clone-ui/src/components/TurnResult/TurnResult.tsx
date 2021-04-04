07e7a72db630b62135bd5d964af4c08122b88ac4
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2021-04-04 14:54:27 +0100

    fix turn result on failure

--- the-one-clone-ui/src/components/TurnResult/TurnResult.tsx
+++ the-one-clone-ui/src/components/TurnResult/TurnResult.tsx
@@ -14,23 +14,24 @@ export default (props: TurnResultProps) => {
     return (
         <div className={styles.turnResults}>
             {turn.result === TurnResultType.success &&
-                <div>
-                    <h1 className={styles.correct}>Correct!</h1>
-                    <div>{player && player.isMe ? "You've" : player && player.name} guessed the word</div>
-                    <div className={styles.highlight}>{turn.secretWord}</div>
-                </div> }
-            {turn.result === TurnResultType.skip ?
-                <div>
-                    <h1>Skipped!</h1>
-                    <div>The word was <span className={styles.highlight}>{turn.secretWord}</span></div>
-                </div>
-                :
-                <div>
-                    <h1>Ohh noo...</h1>
-                    <div>The word was <span className={styles.highlight}>{turn.secretWord}</span></div>
-                    <div>but {player && player.isMe ? "You've" : player && player.name} guessed <span className={`${styles.incorrect} ${styles.highlight}`}>{turn.guess}</span></div>
-                </div>
-
+            <div>
+                <h1 className={styles.correct}>Correct!</h1>
+                <div>{player && player.isMe ? "You've" : player && player.name} guessed the word</div>
+                <div className={styles.highlight}>{turn.secretWord}</div>
+            </div>}
+            {turn.result === TurnResultType.skip &&
+            <div>
+                <h1>Skipped!</h1>
+                <div>The word was <span className={styles.highlight}>{turn.secretWord}</span></div>
+            </div>
+            }
+            {turn.result === TurnResultType.failure &&
+            <div>
+                <h1>Ohh noo...</h1>
+                <div>The word was <span className={styles.highlight}>{turn.secretWord}</span></div>
+                <div>but {player && player.isMe ? "You've" : player && player.name} guessed <span
+                    className={`${styles.incorrect} ${styles.highlight}`}>{turn.guess}</span></div>
+            </div>
             }
         </div>
     )
