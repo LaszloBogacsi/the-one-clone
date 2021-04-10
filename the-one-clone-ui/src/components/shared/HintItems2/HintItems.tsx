db5a378cf9d2b5b7707322fc3f32ef71e1e0991f
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2021-04-11 00:14:57 +0100

    fix comp warning

--- the-one-clone-ui/src/components/shared/HintItems2/HintItems.tsx
+++ the-one-clone-ui/src/components/shared/HintItems2/HintItems.tsx
@@ -16,8 +16,8 @@ export default (props: HintItemProps) => {
             <h3>Hints</h3>
             <div className={styles.hintItems}>
                 {hints.map((hint, index) => {
-                    return <div className={styles.hintItemContainer}>
-                        <div className={`${styles.hintItem} ${hint.duplicate ? styles.duplicate : ""} ${players.find(player => hint.player === player.id)?.color || ""}`} key={index}>
+                    return <div className={styles.hintItemContainer} key={index}>
+                        <div className={`${styles.hintItem} ${hint.duplicate ? styles.duplicate : ""} ${players.find(player => hint.player === player.id)?.color || ""}`}>
                             {hint.hint}
                         </div>
                     </div>
